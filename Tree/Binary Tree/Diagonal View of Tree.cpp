e71a60c4aaee7383ee762bef152260d1a94622fb
GaneshPrasad27 <61357252+GaneshPrasad27@users.noreply.github.com>
2020-08-14 23:37:09 +0530

    Create Diagonal View of Tree.cpp

---
+++ Tree/Binary Tree/Diagonal View of Tree.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int info;
+    Node *left = NULL;
+    Node *right = NULL;
+    int hd; 			// horizontal distance
+};
+
+Node *root = NULL;
+
+Node *newNode(int data) {
+    Node *temp = new Node;
+    temp->info = data;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+
+
+void diagonalView(Node *node) {
+	
+	//base
+    if (node == NULL)
+        return;
+
+    queue<Node *> q;
+    map<int, vector<int> > m;
+    q.push(node);
+    
+    while (!q.empty()) {
+        Node *temp = q.front();
+        q.pop();
+        int hd = temp->hd;
+
+        m[hd].push_back(temp->info); // appends the value to map
+
+        if (temp->left) {
+            temp->left->hd = hd + 1;
+            q.push(temp->left);
+        }
+
+        if (temp->right) {
+            temp->right->hd = hd;
+            q.push(temp->right);
+        }
+
+    }
+    
+    map<int, vector<int> >::iterator itr;
+    for (itr= m.begin(); itr != m.end(); itr++){
+	
+        for (int i = 0; i < itr->second.size(); i++) {
+            cout << itr->second[i] << " ";
+        } cout << endl;
+    }
+}
+
+int main() {
+
+    root = newNode(1);
+	root->left = newNode(2);
+	root->right = newNode(3);
+	root->left->left = newNode(4);
+	root->left->right = newNode(5);
+	root->left->left->left = newNode(6);
+	root->left->left->right = newNode(7);
+   
+    cout<<"Digonal View : \n";
+    diagonalView(root);
+	cout << endl;
+	 
+    return 0;
+}
+
+/* Output:
+
+Digonal View :
+1 3
+2 5
+4 7
+6
+
+*/
