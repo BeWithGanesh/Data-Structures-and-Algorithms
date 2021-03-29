068b8288eb31dd38995cf4d0fc74384913a1e8fb
GaneshPrasad27 <61357252+GaneshPrasad27@users.noreply.github.com>
2021-03-29 14:28:03 +0530

    Create reverse individual words.cpp

---
+++ Array/reverse individual words.cpp
@@ -0,0 +1,46 @@
+// Coded by Ganesh Prasad
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void reverseWords(string str){
+	
+	stack<char> st;
+	
+	for(int i=0;i < str.length() ;i++){
+		
+		if(str[i] != ' '){
+			st.push(str[i]);
+			
+		}
+		
+		else{
+			while(st.empty() == false){
+				
+				cout<<st.top();
+				
+				st.pop();
+			}
+			
+			cout<<" ";
+		}
+		
+		
+	}	
+	
+	 while (st.empty() == false) {
+        cout << st.top();
+        st.pop();
+    }
+	
+}
+
+
+int main(){
+	
+		string str = "hai this is ganesh";
+		
+		reverseWords(str);
+		
+	return 0;
+}
