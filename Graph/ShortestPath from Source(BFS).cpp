658216e67e8ff8a2041224262d9d092ab317e61b
GaneshPrasad27 <to.bewithganesh@gmail.com>
2021-06-24 08:56:09 +0530

    Create ShortestPath from Source(BFS).cpp

---
+++ Graph/ShortestPath from Source(BFS).cpp
@@ -0,0 +1,72 @@
+// Coded by Ganesh Prasad
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void addEdge(vector<int> adj[],int u,int v){
+	
+	adj[u].push_back(v);
+	adj[v].push_back(u);
+	
+}
+
+void shortestPath(vector<int> adj[],int V,int S){
+	
+	int dist[V];
+	for(int i=0;i<V;i++){
+		dist[i] = INT_MAX;
+	}
+	
+	queue<int> q;
+	dist[S]=0;
+	q.push(S);
+	
+	while(!q.empty()){
+		
+		int node = q.front();
+		q.pop();
+		
+		for(int x:adj[node]){
+			if(dist[node]+1 < dist[x]){
+				dist[x] = dist[node]+1;
+				q.push(x);
+			}
+		}
+	}
+	
+	for(int i=0;i<V;i++){
+		cout<<i<<"->"<<dist[i];
+		cout<<endl;
+	}
+	
+}
+
+
+int main(){
+	
+	int V = 5;
+	vector<int> adj[V];
+	addEdge(adj,0,1);
+	addEdge(adj,0,2);
+	addEdge(adj,0,4);
+	addEdge(adj,1,2);
+	addEdge(adj,1,3);
+	addEdge(adj,2,3);
+
+	// function
+	shortestPath(adj,V,0);
+	
+	return 0;
+}
+
+
+
+// Output:
+
+// 0->0
+// 1->1
+// 2->1
+// 3->2
+// 4->1
+//  
